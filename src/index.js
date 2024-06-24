c9312df96b6bf9b063bca6372a99321fba6612e0
F1mc <99305579+F1mc@users.noreply.github.com>
2024-06-24 22:38:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.flmc.me": dockerHub,
+  "quay.flmc.me": "https://quay.io",
+  "gcr.flmc.me": "https://gcr.io",
+  "k8s-gcr.flmc.me": "https://k8s.gcr.io",
+  "k8s.flmc.me": "https://registry.k8s.io",
+  "ghcr.flmc.me": "https://ghcr.io",
+  "cloudsmith.flmc.me": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.flmc.me": dockerHub,
 };
 
 function routeByHosts(host) {
